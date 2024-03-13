e0eeed3207a17596f08191cf075a17029c24ac64
Balu Babu <balub997@gmail.com>
2024-03-13 09:05:55 +0530

    chore: moved global settings to config/globals.lua

---
+++ lua/config/globals.lua
@@ -0,0 +1,3 @@
+
+vim.g.mapleader = " " -- Set space as the leader key
+vim.g.maplocalleader = " " 
--- lua/config/init.lua
+++ lua/config/init.lua
@@ -12,10 +12,8 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
--- Initalize lazy with basic plugins
--- Example using a list of specs with the default options
-vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappings are correct
-vim.g.maplocalleader = "\\" -- Same for `maplocalleader`
+-- Initalize globals
+require('config.globals')
 
 local opts = {
 	defaults = {
